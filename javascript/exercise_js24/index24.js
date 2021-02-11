7aa78f2193e79ced691e9f47a92685a71e3f1167
Herve Nguefack <xnguefack@gmail.com>
2021-02-11 12:46:45 -0800

    just exercise 24 js, next do many exercises and add+commit once

---
+++ javascript/exercise_js24/index24.js
@@ -0,0 +1,6 @@
+// Create a new index24.js file
+// Define a username variable
+let username = null;
+// Show the username variable value using console.log() with the variable name (string) as first parameter and the actual variable value as second
+// Example: console.log('name', name);
+console.log("username:", username); // ok
