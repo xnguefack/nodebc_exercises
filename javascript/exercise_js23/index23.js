f0eb28e06f472000ef42875bbc2a4d17e0df3e6f
Herve Nguefack <xnguefack@gmail.com>
2021-02-11 12:37:21 -0800

    just exercise 23 js

---
+++ javascript/exercise_js23/index23.js
@@ -0,0 +1,6 @@
+// Create a new index23.js file
+// Define a username variable
+let username = "node_developer";
+// Show the username variable value using console.log() with the variable name (string) as first parameter and the actual variable value as second
+// Example: console.log('name', name);
+console.log("username:", username);
